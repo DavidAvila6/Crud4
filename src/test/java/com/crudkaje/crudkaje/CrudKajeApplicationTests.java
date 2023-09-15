e62af0351c4eceeda3dc5df82e6c2a7866b98c36
Sala Golden <golden@academia.loc>
2023-09-14 21:41:25 -0500

    SPRING INITIALIZR

---
+++ src/test/java/com/crudkaje/crudkaje/CrudKajeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudkaje.crudkaje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudKajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
