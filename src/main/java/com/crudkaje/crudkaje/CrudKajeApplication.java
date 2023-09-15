67121cf6343aa7fa16f85b15048974588140b0fe
Sala Golden <golden@academia.loc>
2023-09-14 21:47:01 -0500

    CLON DESARROLLO

---
+++ src/main/java/com/crudkaje/crudkaje/CrudKajeApplication.java
@@ -0,0 +1,13 @@
+package com.crudkaje.crudkaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudKajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudKajeApplication.class, args);
+	}
+
+}
